8252789ccd8c43779f267b9bc53cc05f858da1d6
Scott Bedard <scottbedard90@gmail.com>
2017-11-25 10:52:28 -0700

    add example pages

--- compile.js
+++ compile.js
@@ -15,8 +15,34 @@ getDirectories(path.resolve(__dirname, 'components')).forEach((dir) => {
     // compile the component to an es module
     const output = svelte.compile(source, {
         name: 'Component',
-        format: 'es',
+        format: 'umd',
     });
+
+    fs.writeFileSync(path.resolve(__dirname, dir, 'index.html'), `
+        <!doctype html>
+        <html>
+            <body>
+                <h1><a href="file://${dir}">${dir}</a></h1>
+
+                <ul>
+                    <li><a href="file:///C:/git/svelte-output/components">Components</a></li>
+                    <li><a href="./Component.js">View compiled code</a></li>
+                </ul>
+
+                <hr />
+
+                <div id="component"></div>
+
+                <script type="text/javascript" src="./Component.js"></script>
+
+                <script type="text/javascript">
+                    new Component({
+                        target: document.querySelector('#component'),
+                    });
+                </script>
+            </body>
+        </html>
+    `);
 
     // save the resulting code and provide some feedback that it worked
     fs.writeFileSync(path.resolve(__dirname, dir, 'Component.js'), output.code);
